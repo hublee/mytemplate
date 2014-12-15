5690cb0d9bed6de69ec05bfb8ce60e15b6ea3299
if <if@qq.com>
2014-12-15 19:35:30 +0800

    Signed-off-by: if <if@qq.com>

---
+++ mytemplate/java/com/template/common/base/BaseMybatis.java
@@ -0,0 +1,7 @@
+package com.template.common.base;
+
+import com.template.common.mybatis.mapper.Mapper;
+
+public abstract class BaseMybatis<T extends Entity> implements Mapper<T> {
+	
+}
---
+++ mytemplate/java/com/template/web/sys/controller/DictController.java
@@ -0,0 +1,50 @@
+package com.template.web.sys.controller;
+
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.template.web.sys.model.SysDict;
+import com.template.web.sys.service.SysDictService;
+
+@Controller
+@RequestMapping("${adminPath}/dict")
+public class DictController {
+
+	@Resource
+	private SysDictService sysDictService;
+	
+	@RequestMapping
+	public String toDict(Model model){
+		return "sys/dict/dict";
+	}
+	
+	/**
+	 * 添加或更新区域
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public @ResponseBody Integer save(@ModelAttribute SysDict sysDict) {
+		return sysDictService.saveSysdict(sysDict);
+	}
+	
+	/**
+	 * 分页显示字典table
+	 * @param params
+	 * @return
+	 */
+	/*@RequestMapping(value = "/list", method = RequestMethod.POST)
+	public String list(@RequestParam Map<String, Object> params, Model model) {
+		PageInfo<SysArea> page = sysAreaService.findPageInfo(params);
+		model.addAttribute("page", page);
+		return "sys/area/area-list";
+	}*/
+	
+}
