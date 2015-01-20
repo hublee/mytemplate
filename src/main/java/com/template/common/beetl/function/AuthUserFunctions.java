57e36c05eeb91227f99ac5fff44f146028a85832
if <if@qq.com>
2015-01-20 21:01:17 +0800

    增加按钮级别权限，请查看菜单配置-区域管理节点下的东东的备注说明

--- src/main/java/com/template/common/beetl/function/AuthUserFunctions.java
+++ src/main/java/com/template/common/beetl/function/AuthUserFunctions.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.template.common.beetl.util.BeetlUtils;
+import com.template.common.constant.Constant;
 import com.template.web.sys.model.SysResource;
 import com.template.web.sys.utils.SysUserUtils;
 
@@ -14,10 +16,16 @@ public class AuthUserFunctions {
 	 * 判断用户是否具有指定权限
 	 */
 	public boolean hasPermission(String url) {
+		Map<String, SysResource> allRes = BeetlUtils
+				.getBeetlSharedVars(Constant.CACHE_ALL_RESOURCE);
+		SysResource sysResource = allRes.get(url);
+		if (sysResource == null
+				|| Constant.RESOURCE_COMMON.equals(sysResource.getCommon())) {
+			return true;
+		} 
 		Map<String, SysResource> userRes = SysUserUtils
 				.getUserPermission(SysUserUtils.getSessionUser());
-		if (userRes.containsKey(url))
-			return true;
+		if (userRes.containsKey(url)) return true;
 		return false;
 	}
 
