592d6431a136fb0af0f93b423587bff68b88acc0
if <if@>
2015-02-26 13:47:17 +0800

    Signed-off-by: if <if@>

--- src/main/java/com/template/common/spring/aspect/SystemLogAspect.java
+++ src/main/java/com/template/common/spring/aspect/SystemLogAspect.java
@@ -1,14 +1,24 @@
 package com.template.common.spring.aspect;
 
-import javax.annotation.Resource;
+import java.lang.reflect.Method;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import com.template.common.spring.annotation.SystemControllerLog;
+import com.template.common.utils.IPUtils;
+import com.template.web.sys.model.SysLog;
 import com.template.web.sys.service.SysLogService;
+import com.template.web.sys.utils.SysUserUtils;
 
 @Aspect
 @Component
@@ -22,8 +32,49 @@ public class SystemLogAspect {
 			.getLogger(SystemLogAspect.class);
 
 	// Controller层切点
-	@Pointcut("@annotation(com.annotation.SystemControllerLog)")
+	@Pointcut("@annotation(com.template.common.spring.annotation.SystemControllerLog)"
+			+ " || execution(* com.template.web..*Controller.save*(..))"
+			+ " || execution(* com.template.web..*Controller.delete*(..))")
 	public void controllerAspect() {
+	}
+
+	@Pointcut("@annotation(com.template.common.spring.annotation.SystemServiceLog)")
+	public void serviceAspect() {
+	}
+
+	@AfterReturning(value = "controllerAspect()", returning = "rtv")
+	public void doAfterReturning(JoinPoint joinPoint, Object rtv) {
+		try {
+			HttpServletRequest request = SysUserUtils.getCurRequest();
+			SysLog log = new SysLog();
+			// 判断参数
+			if (joinPoint.getArgs() != null) {
+				StringBuffer rs = new StringBuffer();
+				for (int i = 0, len = joinPoint.getArgs().length; i < len; i++) {
+					Object info = joinPoint.getArgs()[i];
+					if (info != null) {
+						String paramName = info.getClass().getSimpleName();
+						rs.append("[参数" + (i + 1) + "，类型:" + paramName + "，值:"
+								+ info.toString() + "]");
+					} else {
+						rs.append("[参数" + (i + 1) + "，值:null]");
+					}
+				}
+				log.setParams(rs.toString());
+			}
+			log.setRemoteAddr(IPUtils.getClientAddress(request));
+			log.setRequestUri(request.getRequestURI());
+			log.setMethod(request.getMethod());
+			log.setUserAgent(request.getHeader("user-agent"));
+			Method m = ((MethodSignature) joinPoint.getSignature()).getMethod();
+			SystemControllerLog sclog = m
+					.getAnnotation(SystemControllerLog.class);
+			if (sclog != null)
+				log.setDescription(sclog.description());
+			sysLogService.save(log);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
 	}
 
 }
