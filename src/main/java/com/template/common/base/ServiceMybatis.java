13a3821c0753015893dd1272df40c64c0ef40520
if <if@qq.com>
2015-03-03 23:41:36 +0800

    Signed-off-by: if <if@qq.com>

--- src/main/java/com/template/common/base/ServiceMybatis.java
+++ src/main/java/com/template/common/base/ServiceMybatis.java
@@ -51,16 +51,6 @@ public abstract class ServiceMybatis<T extends BaseEntity> implements BaseServic
 		Example example = new Example(record.getClass());
 		example.setOrderByClause(orderSqlStr);
 		List<Map<String, Object>> mapList = commonMapper.selectByExample(record.getClass(), example);
-		/*List<T> newList = new ArrayList<T>();
-		for(Map<String, Object> map : mapList){
-			try {
-				T t = (T) record.getClass().newInstance();
-				t.setAll(map);
-				newList.add(t);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}*/
 		return (List<T>) Collections3.maplist2EntityMapList(mapList, record.getClass());
 	}
 
