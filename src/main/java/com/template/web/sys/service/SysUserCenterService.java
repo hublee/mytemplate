7b77817b23278f3560846affb7daa4b310556549
if <if@qq.com>
2015-03-18 22:34:18 +0800

    u

--- src/main/java/com/template/web/sys/service/SysUserCenterService.java
+++ src/main/java/com/template/web/sys/service/SysUserCenterService.java
@@ -6,8 +6,10 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.alibaba.druid.util.StringUtils;
 import com.template.common.base.ServiceMybatis;
 import com.template.common.beetl.function.OfficeFunctions;
+import com.template.common.utils.IPUtils;
 import com.template.web.sys.model.SysOffice;
 import com.template.web.sys.model.SysUser;
 import com.template.web.sys.utils.SysUserUtils;
@@ -24,13 +26,17 @@ public class SysUserCenterService extends ServiceMybatis<SysUser>{
 		Long companyId = sysUser.getCompanyId();
 		Long officeId = sysUser.getOfficeId();
 		String orgStr = null;
-		if (companyId.equals(officeId)) {
+		if (companyId.equals(officeId)) { //机构
 			orgStr = ((SysOffice) offices.get(companyId)).getName();
 		} else {
 			orgStr = ((SysOffice) offices.get(companyId)).getName() + " —— "
 					+ ((SysOffice) offices.get(officeId)).getName();
 		}
+		String curIP = IPUtils.getClientAddress(SysUserUtils.getCurRequest());
+		String ipEx = ""; 
+		if(!StringUtils.equals(sysUser.getLoginIp(), curIP)) ipEx = "(当前IP:"+curIP+"，请注意!)";
 		sysUser.set("orgStr", orgStr);
+		sysUser.set("ipEx", ipEx);
 		return sysUser;
 	}
 	
