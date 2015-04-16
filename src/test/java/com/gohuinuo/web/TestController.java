9e0087c277aa9eaf76733362df1e34e616e06015
if <if@fly>
2015-04-16 21:29:03 +0800

    双列表插件

--- src/test/java/com/gohuinuo/web/TestController.java
+++ src/test/java/com/gohuinuo/web/TestController.java
@@ -2,6 +2,7 @@ package com.gohuinuo.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
@@ -9,6 +10,11 @@ public class TestController {
 	@RequestMapping("test")
 	public String test(){
 		return "test";
+	}
+	
+	@RequestMapping(value="duallist")
+	public @ResponseBody void test1(String[] duallist){
+		System.out.println(duallist);
 	}
 	
 }
